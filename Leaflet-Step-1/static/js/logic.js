d08fd651b030a8c3731c38b64980955abb35f21b
YannChye <yannying.chye@monash.edu>
2020-09-11 17:33:02 +1000

    add gitignore

---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,2 @@
+//past 30 days' M1.0+ earthquakes (https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php)
+var url="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_month.geojson"
